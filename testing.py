b9de9eb24eebfafbc941565105377d74ad3d5edc
Declan Quinn <98410755+declanquinn00@users.noreply.github.com>
2022-10-01 13:35:04 +0100

    Update testing.py

--- testing.py
+++ testing.py
@@ -3,8 +3,8 @@ from numericOperation import multiply_op
 
 def test_mult():
   assert multiply_op(10,11) == 110
-def test_div():
-  assert divide_op(2,1) == 2
+#def test_div():
+#  assert divide_op(2,1) == 2
 def test_add():
   assert add_op(10,11) == 21
 def test_sub():
